2dd8088ff69255ee33129db1a3490e8fe9166389
dimash <diamsh101@gmail.com>
2025-12-14 23:42:17 +0100

    done

--- problem_1/sample_solution.cpp
+++ problem_1/sample_solution.cpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <algorithm>
 #include <iomanip>
-
+#include <thread>
 
 std::vector<std::vector<double>> read_matrix() {
     size_t rows, cols;
@@ -43,32 +43,46 @@ std::vector<std::vector<double>> read_matrix() {
     return result;
 }
 
-
+std::vector<std::thread> t;
+std::mutex mtx;
+std::vector<std::vector<double>> result;
+std::vector<std::vector<double>> left, right;
+void solve(int start_i, int step, int left_rows, int right_cols, int left_cols) {
+    for(int i = start_i; i < left_rows; i += step) {
+        for (int j = 0; j < right_cols; ++j) {
+            double sum = 0;
+            for (int k = 0; k < left_cols; ++k) {
+//                mtx.lock();
+                sum += left[i][k] * right[k][j];
+//                mtx.unlock();
+            }
+            result[i][j] = sum;
+        }
+    }
+}
 int main() {
-    auto left = read_matrix();
-    auto right = read_matrix();
+    unsigned int num_threads = std::thread::hardware_concurrency();
+    left = read_matrix();
+    right = read_matrix();
     auto left_rows = left.size();
     auto left_cols = left[0].size();
     auto right_cols = right[0].size();
 
-    if (left.empty() || right.empty() || left[0].size() != right.size()) {
+    if (left.empty() or right.empty() or left[0].size() != right.size()) {
         std::cerr << "Wrong matrices";
         return 1;
     }
-
-    std::vector<std::vector<double>> result(left_rows, std::vector<double>(right_cols));
-    for (int i = 0; i < left_rows; ++i) {
-        for (int j = 0; j < right_cols; ++j) {
-            for (int k = 0; k < left_cols; ++k) {
-                result[i][j] += left[i][k] * right[k][j];
-            }
-        }
+    result.assign(left_rows, std::vector<double>(right_cols));
+    for(int i = 0; i < num_threads; i++) {
+        t.emplace_back(solve, i, num_threads, left_rows, right_cols, left_cols);
     }
-
+    for(auto& i : t) {
+        i.join();
+    }
     std::cout << left_rows << ' ' << right_cols << "\n";
     for (int i = 0; i < left_rows; ++i) {
         for (int j = 0; j < right_cols; ++j) {
-            std::cout << std::setprecision(12) << result[i][j] << ' ';
+            std::cout << std::setprecision(15) << result[i][j] << ' ';
         }
         std::cout << "\n";
     }
